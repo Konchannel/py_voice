846bc2b531de8e95ffddd965e089f590ee5dbae4
Konchannel <konchan2416@gmail.com>
2019-12-24 23:00:33 +0900

    add: add words of python_format

--- edit_message.py
+++ edit_message.py
@@ -7,9 +7,12 @@ def edit_py_format(message):
                         '中括弧': '{', '中カッコ': '{',)
     '''
 
+    message = re.sub('かくかっことじる|角括弧とじる|角括弧閉じる', ']', message)
+    message = re.sub('角括弧|角カッコ', '[', message)
+    message = re.sub('中学校とじる|中学校閉じる|中括弧とじる|中括弧閉じる', '}', message)
+    message = re.sub('中学校|中括弧|中カッコ', '{', message)
     message = re.sub('かっことじる|括弧とじる|括弧閉じる', ')', message)
     message = re.sub('かっこ|括弧', '(', message)
-    message = re.sub('角括弧|角カッコ', '[', message)
     message = re.sub('プリント', 'print', message)
     message = re.sub('ダブルクォーテーション', '"', message)
     message = re.sub('クォーテーション', "'", message)
