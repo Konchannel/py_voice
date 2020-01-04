d941a77aa8b7885d9bc32415ad6e61646a6e15fa
Konchannel <konchan2416@gmail.com>
2020-01-05 02:36:34 +0900

    fix: convert 'かっこ閉じる' to ')'

--- edit_message.py
+++ edit_message.py
@@ -6,7 +6,7 @@ def edit_py_format(message):
     message = re.sub('角括弧|角カッコ', '[', message)
     message = re.sub('中学校とじる|中学校閉じる|中括弧とじる|中括弧閉じる', '}', message)
     message = re.sub('中学校|中括弧|中カッコ', '{', message)
-    message = re.sub('かっことじる|括弧とじる|括弧閉じる', ')', message)
+    message = re.sub('かっことじる|かっこ閉じる|括弧とじる|括弧閉じる', ')', message)
     message = re.sub('かっこ|括弧', '(', message)
     message = re.sub('プリント', 'print', message)
     message = re.sub('ダブルクォーテーション', '"', message)
