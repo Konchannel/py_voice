54e4bb8fe9829d498b600aef26548623fc850300
Konchannel <konchan2416@gmail.com>
2020-01-05 02:31:47 +0900

    refactoring:pass message as an class-argument when declaring a class

--- gui.py
+++ gui.py
@@ -2,27 +2,42 @@
 
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.lang import Builder
+
+setting = ("""
+BoxLayout:
+    Label:
+        text:
+            "hogehoge"
+        text_size:
+            self.width, self.height
+        halign:
+            'left'
+        valign:
+            'top'
+        size_hint_x:
+            1
+""")
 
 
 def print_gui(get_message):
-    ta = TestApp()
+    if get_message is "":
+        get_message = "not input message"
 
-    if get_message is not "":
-            ta.set_message(get_message)
+    ta = TestApp(get_message)
 
     ta.run()
 
 
 class TestApp(App):
-    def __init__(self):
-        self.message = "not input message"
+    def __init__(self, get_message):
         super(TestApp, self).__init__()
+        self.message = get_message
 
     def build(self):
         self.title = 'py_voice'
-        return Label(text=self.message, font_name="C:\WINDOWS\FONTS\MEIRYO.TTC",
-                     text_size=(700, 600), halign='left', valign='top')
+        la = Label(text=self.message, font_name="C:\WINDOWS\FONTS\MEIRYO.TTC", color=(1, 0, 0, 1),
+                   text_size=(700, 600), halign='left', valign='top')
 
-    def set_message(self, message):
-        self.message = message
-
+        # return Builder.load_string(setting)
+        return la
